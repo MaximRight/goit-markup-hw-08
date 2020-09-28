32e6c10232eeb3695cc69483d07651ebf48c22d6
Maxim Right <maxright1984@gmail.com>
2020-09-28 14:12:46 +0300

    Гибридная Адаптивка, 3 перелома, далее адаптивные картинки и шрифты

--- js/modal-body.js
+++ js/modal-body.js
@@ -2,15 +2,13 @@
 	const refs = {
 		openModalBtn: document.querySelector("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
-		// overflow: document.querySelector("[data-overflow]"),
+		overflow: document.querySelector("[data-overflow]"),
 	}
 
 	refs.openModalBtn.addEventListener("click", toggleModal)
 	refs.closeModalBtn.addEventListener("click", toggleModal)
 
 	function toggleModal() {
-		document.body.classList.toggle("modal-open")
-		refs.modal.classList.toggle("is-hidden")
+		refs.overflow.classList.toggle("overflow")
 	}
 })()
